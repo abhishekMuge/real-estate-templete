e320228d9447301de4a0a8db629d09edd5d80b81
abhishekMuge <muge111hhh@gmail.com>
2021-08-09 15:51:10 +0530

    9/8/21 - work updates(banner section)

--- assets/js/Site.js
+++ assets/js/Site.js
@@ -2,11 +2,39 @@
 
 let banner = $(".banner-area");
 let bannerHeading = $(".banner-content-heading");
+let tabOption = $(".tab-option");
 
 $(".property-banner").click(function () {
   $(".banner-area").addClass("banner-one");
   $(".banner-area").removeClass("banner-two banner-three");
   bannerHeading.text("Home Is The Starting Place Of Love, Hope And Dreams");
+
+  tabOption.html(`
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+        <label class="form-check-label white" for="inlineCheckbox1">
+          New Booking
+            <span class="sub-text">
+              (New Launch)
+            </span>
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+          <label class="form-check-label white" for="inlineCheckbox1">
+            Pre-Booking
+            <span class="sub-text">
+              (Under Construction)
+            </span>
+          </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+          <label class="form-check-label white" for="inlineCheckbox1">
+            Ready To Move
+          </label>
+      </div>
+  `);
 
   $(".project-banner").removeClass("active-pill");
   $(".dealers-banner").removeClass("active-pill");
@@ -19,6 +47,21 @@ $(".project-banner").click(function () {
 
   bannerHeading.text("Select Your Comfort Home From Our New Collection");
 
+  tabOption.html(`
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+        <label class="form-check-label white" for="inlineCheckbox1">
+          Rent
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+          <label class="form-check-label white" for="inlineCheckbox1">
+            Leave
+          </label>
+      </div>
+  `);
+
   $(".property-banner").removeClass("active-pill");
   $(".dealers-banner").removeClass("active-pill");
   $(".project-banner").addClass("active-pill");
@@ -30,7 +73,60 @@ $(".dealers-banner").click(function () {
 
   bannerHeading.text("We Built Your Home To Keep Safe And Secure");
 
+  tabOption.html(`
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+        <label class="form-check-label white" for="inlineCheckbox1">
+          Invesment
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+          <label class="form-check-label white" for="inlineCheckbox1">
+            Consultancy
+          </label>
+      </div>
+  `);
+
   $(".property-banner").removeClass("active-pill");
   $(".project-banner").removeClass("active-pill");
   $(".dealers-banner").addClass("active-pill");
 });
+
+(function () {
+  var parent = document.querySelector(".price-slider");
+  if (!parent) return;
+
+  var rangeS = parent.querySelectorAll("input[type=range]"),
+    numberS = parent.querySelectorAll("input[type=number]");
+
+  rangeS.forEach(function (el) {
+    el.oninput = function () {
+      var slide1 = parseFloat(rangeS[0].value),
+        slide2 = parseFloat(rangeS[1].value);
+
+      if (slide1 > slide2) {
+        [slide1, slide2] = [slide2, slide1];
+      }
+
+      numberS[0].value = slide1;
+      numberS[1].value = slide2;
+    };
+  });
+
+  numberS.forEach(function (el) {
+    el.oninput = function () {
+      var number1 = parseFloat(numberS[0].value),
+        number2 = parseFloat(numberS[1].value);
+
+      if (number1 > number2) {
+        var tmp = number1;
+        numberS[0].value = number2;
+        numberS[1].value = tmp;
+      }
+
+      rangeS[0].value = number1;
+      rangeS[1].value = number2;
+    };
+  });
+})();
